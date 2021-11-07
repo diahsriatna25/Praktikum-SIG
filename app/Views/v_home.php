a410c83faf2a0d12dca60ab17392a10cc37571b1
diah <diahoppo3737@gmail.com>
2021-11-07 23:37:55 +0700

    Tugas

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,33 +1,43 @@
-
-<!DOCTYPE html>
-
-<html lang="en"
-	prefix="og: https://ogp.me/ns#" >
-
-<head>
-
-
-	<meta charset="UTF-8">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	
-
-	<title> Das Blog</title>
-	
-    <div class="container mt-3">
+<div class="container mt-3">
     <h3 class="text-center">Selamat Datang di Das Blog</h3>
 </div>
 </div>
-<br><br>
-<div class="hero">
-	<div class="hero-inner">
-        <aside id="primer-hero-text-2" class="widget widget_text primer-widgets primer-hero-text-widget">		
-            <div class="textwidget primer-widgets primer-hero-text-widget">
-				<p class="widget-title">Praktikum Mata Kuliah Web Lanjut</p>
-                <ul><li>Connecting Korea to the world</li></ul>
-				<p>Connecting Korea to the world,<br />one movie at a time.</p>
+<hr>
+<br>
 
-</body>
+<div class="container mt-3">
+<article class="pexcerpt1 post excerpt last">
+    <a href="https://dzofar.com/2021/08/16/5-kelebihan-memesan-hotel-di-jogja-untuk-staycation-lewat-traveloka/" title="5 Kelebihan Memesan Hotel di Jogja untuk Staycation Lewat Traveloka" rel="nofollow" id="featured-thumbnail">
+    <div class="featured-thumbnail"><img width="270" height="180" src="https://dzofar.com/wp-content/uploads/sambi-resort-spa-1-220x162.jpeg" class="attachment-featured size-featured wp-post-image" alt="" loading="lazy" title="" /></div>									
+    <div class="featured-cat"></div>
+</a>
+<header>	
+    <br>					
+    <h4 class="title"><a href="https://dzofar.com/2021/08/16/5-kelebihan-memesan-hotel-di-jogja-untuk-staycation-lewat-traveloka/" title="5 Kelebihan Memesan Hotel di Jogja untuk Staycation Lewat Traveloka" rel="bookmark">5 Kelebihan Memesan Hotel di Jogja untuk Staycation Lewat Traveloka</a></h4>
+</header><!--.header-->
+<div class="post-content image-caption-format-1">
+    Selain Bali, Jogja menjadi salah satu destinasi wisata di Indonesia yang menarik banyak wisatawan. Kombinasi alam, budaya dan juga kuliner yang dimiliki oleh Jogja benar-benar menyihir banyak orang yang datang. Apalagi wilayah yang memiliki julukan sebagai Kota Pelajar ini juga menawarkan banyak sekali hotel-hotel untuk staycation.
+</article>
+</div>
+</div>
 
-</html>
+<br>
+<div class="container mt-3">
+<article class="pexcerpt6 post excerpt ">
+    <a href="https://dzofar.com/2021/06/15/ini-9-wisata-alam-banten-dengan-view-eksotis-wajib-kesini/" title="Ini 9 Wisata Alam Banten Dengan View Eksotis, Wajib Kesini!" rel="nofollow" id="featured-thumbnail">
+    <div class="featured-thumbnail"><img width="270" height="180" src="https://dzofar.com/wp-content/uploads/Pantai-Cibeureum.-220x162.jpg" class="attachment-featured size-featured wp-post-image" alt="" loading="lazy" title="" /></div></a>
+
+    <header><br>						
+        <h4 class="title">
+            <a href="https://dzofar.com/2021/06/15/ini-9-wisata-alam-banten-dengan-view-eksotis-wajib-kesini/" title="Ini 9 Wisata Alam Banten Dengan View Eksotis, Wajib Kesini!" rel="bookmark">Ini 9 Wisata Alam Banten Dengan View Eksotis, Wajib Kesini!</a>
+		</h4>
+	</header><!--.header-->
+    
+    <div class="post-content image-caption-format-1">
+        Banten merupakan provinsi yang berbatasan dengan DKI Jakarta dan juga Jawa Barat. Letaknya yang berada di ujung paling Barat pulau Jawa membuatnya memiliki wilayah laut. Namun pun begitu, wilayah daratannya juga memiliki pegunungan dan perbukitan. Dengan demikian bisa dikatakan, Banten itu tempat yang lengkap. Tempat wisata yang ada di sana meliputi pantai, laut, danau, hingga...
+	</div>
+</article>
+</div>
+</div>
+<hr>
+
